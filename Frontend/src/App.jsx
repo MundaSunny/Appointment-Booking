cd45fc400708a164d8504c41e6ac5ba4e7bc3ded
MundaSunny <mundasunny99@gmail.com>
2025-04-25 20:57:06 +0530

    Added new pages and updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react'
-
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Doctor from './pages/Doctors'
 const App = () => {
   return (
-    <div >
-     
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/doctor' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }

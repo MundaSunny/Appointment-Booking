0382b216ad00e9c881c2fe27c284b62ad7167295
MundaSunny <mundasunny99@gmail.com>
2025-04-26 20:32:57 +0530

    updated app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,19 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
-import Doctor from './pages/Doctors'
+import Doctors from './pages/Doctors'
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
       <Routes>
         <Route path='/' element={<Home/>}/>
-        <Route path='/doctor' element={<Home/>}/>
+        <Route path='/doctor' element={<Doctors/>}/>
+        <Route path='/doctor/:speciality' element={<Doctors/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/my-profile' element={<MyProfile/>}/>
+        <Route path='/my-appointment' element={<MyAppointment/>}/>
       </Routes>
     </div>
   )

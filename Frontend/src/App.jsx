34fd1c0948fa5d3469e12d94441a514f0e40b49f
MundaSunny <mundasunny99@gmail.com>
2025-04-29 11:02:57 +0530

    updated and added Navbar in components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Doctors from './pages/Doctors'
+import Navbar from './components/Navbar'
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/doctor' element={<Doctors/>}/>
@@ -15,6 +17,7 @@ const App = () => {
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/my-profile' element={<MyProfile/>}/>
         <Route path='/my-appointment' element={<MyAppointment/>}/>
+        <Route path='/appointment/:docId' element={<Appointment/>}/>
       </Routes>
     </div>
   )
